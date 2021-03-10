949aea9c03e14ee8de18a6678b3327cd740826e7
Cihangir Cengiz <cihangir.cengiz@broadangle.com>
2021-03-10 18:06:46 +0300

    #4639 added image to copyright

--- components/CopyrightInfo/CopyrightInfo.js
+++ components/CopyrightInfo/CopyrightInfo.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import classes from './CopyrightInfo.scss';
+import poweredByDrivrz from './powered-by-drivrz-text.png';
+
 
 class CopyrightInfo extends Component {
   constructor(props) {
@@ -11,6 +13,7 @@ class CopyrightInfo extends Component {
     return (
       <div>
         <div className={classes.copyrightInfo}>
+          <div><img src={poweredByDrivrz} className={classes.poweredBy} /></div>
           <div><p className={classes.label}>{this.props.label}</p></div>
           <div><a href="" rel="noopener noreferrer" target="_blank">Terms</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="" rel="noopener noreferrer" target="_blank">Privacy</a></div>
         </div>
